672281d2bc1d193aa5c2080cfffc9c1ee8cfce3e
jiangyy <yyecej@163.com>
2019-09-17 12:03:51 +0800

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import ActivityPage from './View/Pages/ActivityPage'
+
+AppRegistry.registerComponent('DEmos', () => ActivityPage);
